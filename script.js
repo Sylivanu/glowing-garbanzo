a37c6a92687f48172097423d43403d107ca54cb9
Silva <123529015+Sylivanu@users.noreply.github.com>
2024-05-28 22:56:08 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let d = new Date();
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>";
-
-// Add technology effect background
-let techBackground = document.createElement("div");
-techBackground.className = "tech-background";
-document.body.appendChild(techBackground);
